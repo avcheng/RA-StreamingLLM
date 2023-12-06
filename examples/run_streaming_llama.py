a12f5f99a565ba2c3b028d25cd61ce43a483cc50
Alex Cheng <alexcheng@college.harvard.edu>
2023-12-06 14:29:56 -0500

    Update prompt and context

--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -28,6 +28,7 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
         past_key_values=past_key_values,
         use_cache=True,
     )
+    output = ""
     past_key_values = outputs.past_key_values
     pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
     generated_ids = [pred_token_idx.item()]
@@ -54,13 +55,15 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
 
         now = len(generated_text) - 1
         if now > pos:
+            output += " ".join(generated_text[pos:now])
             print(" ".join(generated_text[pos:now]), end=" ", flush=True)
             pos = now
 
         if pred_token_idx == tokenizer.eos_token_id:
             break
+    output += " ".join(generated_text[pos:])
     print(" ".join(generated_text[pos:]), flush=True)
-    return past_key_values
+    return past_key_values, output
 
 
 @torch.no_grad()
@@ -91,11 +94,12 @@ def streaming_inference(model, tokenizer, prompts, service_context=None, kv_cach
         index.insert_nodes(nodes)
 
         # Get context from llama index
-        query_engine = index.as_query_engine(response_mode="compact", service_context=service_context)
+        query_engine = index.as_query_engine(response_mode="compact", service_context=service_context, verbose=False)
         context = query_engine.query(prompt)
+        # print(context)
 
 
-        prompt = "USER: Using the context: " + context + ". Answer the prompt: " + prompt + "\n\nASSISTANT: "
+        prompt = "USER: Answer the prompt: " + prompt + ". Using the following context: " + str(context) + "\n\nASSISTANT: "
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
         input_ids = input_ids.to(model.device)
         seq_len = input_ids.shape[1]
@@ -103,9 +107,16 @@ def streaming_inference(model, tokenizer, prompts, service_context=None, kv_cach
             space_needed = seq_len + max_gen_len
             past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
 
-        past_key_values = greedy_generate(
+        past_key_values, output = greedy_generate(
             model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
         )
+
+        new_document = Document(text=output)
+        nodes = parser.get_nodes_from_documents([new_document])
+
+        # Add to llama index
+        index.insert_nodes(nodes)
+        print("Assistant finished.")
 
 
 def main(args):
